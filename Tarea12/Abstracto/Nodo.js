4ee2fc85a59fee278d09f378da2bf47b1b2b1e41
Selvin Lisandro <36603066+SelvinLP@users.noreply.github.com>
2020-09-04 21:24:15 -0600

    Terminado

--- Tarea12/Abstracto/Nodo.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.cont = void 0;
-exports.cont = { cont: -1 };
--- Tarea12/main.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var parser = require('./Gramatica/Gramatica');
-var salida = parser.parse("2*3+6-1/8");
-var resultado = salida.ejecutar();
-console.log(resultado.cd3);
